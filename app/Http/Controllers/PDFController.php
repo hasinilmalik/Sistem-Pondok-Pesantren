ed501db75869053d1d6e91c2bd827d25b346b325
hasinilmalik <hasinilmalik@gmail.com>
2022-04-12 23:04:46 +0700

    revisi class dompdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
-use Barryvdh\DomPDF\Facade\PDF;
+
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Http\Request;
 
 class PDFController extends Controller
@@ -19,6 +20,6 @@ class PDFController extends Controller
         $data = Student::findOrFail($id);
         $pdf = PDF::loadView('pdf.mou', compact('data'));
         return $pdf->stream('filename.pdf');
-        // return $pdf->download('mou-'.$data->nama.'.pdf');
+        // return $pdf->download('mou-'.$data->nama.'.pdf');    
     }
 }
