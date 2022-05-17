e876702db518a34ed90540993964942f863d8331
hasinilmalik <hasinilmalik@gmail.com>
2022-05-17 07:22:26 +0700

    ok

--- resources/views/livewire/ui/cek-wa.blade.php
+++ resources/views/livewire/ui/cek-wa.blade.php
@@ -1,18 +1,14 @@
 <div>
     <button wire:click='cekWa' class="btn btn-primary btn-sm">Cek Whatsapp</button> <br>
     <br> 6285233002598
-    @if ($wa2 == 1)
+    @if ($wa2)
         <span class="badge badge-pill bg-gradient-success"></span>
-    @elseif($wa2 == 0)
-        <span class="badge badge-pill bg-gradient-danger"></span>
     @else
         <span class="badge badge-pill bg-gradient-secondary"></span>
     @endif
     <br> 6285333920007
-    @if ($wa1 == 1)
+    @if ($wa1)
         <span class="badge badge-pill bg-gradient-success"></span>
-    @elseif($wa1 == 0)
-        <span class="badge badge-pill bg-gradient-danger"> </span>
     @else
         <span class="badge badge-pill bg-gradient-secondary"></span>
     @endif
