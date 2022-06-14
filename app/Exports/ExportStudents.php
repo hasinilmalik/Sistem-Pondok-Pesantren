5e756558d5a7e8fee2147e215a3274b9f6b42e0f
hasinilmalik <hasinilmalik@gmail.com>
2022-06-14 11:28:54 +0700

    ok

--- app/Exports/ExportStudents.php
+++ app/Exports/ExportStudents.php
@@ -12,7 +12,7 @@ class ExportStudents implements FromView
     public function view(): View
     {
         $students = Student::with('madin_institution')->get();
-        dd($students);
+
         return view('export.students',compact('students'));
     }
 }
